01499061e95d0f2221445fa73f71fefdece70374
92brilliantface <92brilliantface@gmail.com>
2016-09-28 13:47:14 +0900

    First upload

---
+++ Ext_changer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import glob
+import os
+
+files = glob.glob("E:/task/new/*") #특정확장자를 가진 목록뽑기
+for x in files:
+    if not os.path.isdir(x): 
+        filename = os.path.splitext(x)
+        os.rename(x, filename[0]  + '.jpg')
